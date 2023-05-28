1e8e70cb24c0160d399c3aed179bfcb69d7bc3a6
aliksab <alexeysavich1112@gmail.com>
2023-05-28 23:27:58 +0900

    backend

---
+++ index.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import multer from 'multer';
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
+import checkAuth from './utils/checkAuth.js';
+import handleValidationErrors from './utils/handleValidationErrors.js';
+import * as UserController from './controllers/UserController.js'
+import * as PostController from './controllers/PostController.js'
+import { database } from './database.js';
+
+mongoose
+    .connect(database)
+    .then(() => console.log('DB ok'))
+    .catch((err) => console.log('DB error', err));
+
+const app = express();
+
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (_, file ,cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use(cors());
+app.use('/uploads', express.static('uploads'));
+
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
+app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`
+    });
+});
+
+app.get('/tags', PostController.getLastTags);
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/tags', PostController.getLastTags);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log('server ok');
+});
